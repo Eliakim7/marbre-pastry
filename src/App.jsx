dfad17a9f03dd90ec51ba6e12785b88fcdd9727c
Eliakim Maillot <eliakim.maillot7@gmail.com>
2023-08-23 11:17:44 +0200

    ajout vercel

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Elia</h1>
+      <h1>Vite + Eliakim</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
